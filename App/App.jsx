cd9609550f7d82daa306e50c972bf7492176450c
joaquimbernaltes <51153324+joaquimbernaltes@users.noreply.github.com>
2021-05-22 20:16:31 +0200

    Insereix a la llista

--- App/App.jsx
+++ App/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import WishInput from './WishInput';
 import WishList from './WishList';
 
 
 //Array amb objectes
-const wishes = [
+const Initialwishes = [
     { text: 'Anar de vacacions i desconnectar de tot', done: false},
     { text: 'Fer deport', done: true},
     { text: 'Comprar un Tesla', done: false},
@@ -15,15 +15,18 @@ const wishes = [
     { text: 'Tindre parella', done: false},
 ]
 
-const App = () => (
+const App = () => {
+    const [wishes, setWishes] = useState(Initialwishes);
+    return(
     <div className="app">
         <h1>Pràctica 1 en React</h1>
         {/* Imprimeix el input de WishInput */}
-        <WishInput />
+        <WishInput onNewWish={wish => setWishes([wish,...wishes])}/>
         
         <WishList wishes={wishes} />
         <button className="wish-clear" type="button">Guardar</button>
     </div>
-);
+    );
+};
 
 export default App;
--- App/WishInput/WishInput.jsx
+++ App/WishInput/WishInput.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 //Funció que tindrà l input per a afegir
-const WishInput = () => (
+//onNewWish es l'event quan se cree algo
+const WishInput = ({ onNewWish }) => {
+    const [newWishText, setNewWishText] = useState('');
+    return(
     <fieldset className="wish-input">
             <legend className="wish-input__label">Introduiex un nou dessitg</legend>
-            <input className="wish-input__field" placeholder="Introdueix el teu dessitg aqui  " />
+            <input 
+                className="wish-input__field" 
+                placeholder="Introdueix el teu dessitg aqui" 
+                value={newWishText} 
+                onChange={e => setNewWishText(e.target.value)} 
+                onKeyUp={(e) => {
+                    if(e.key == 'Enter' && newWishText.length){
+                        //Aqui rebrem el text del seleccionat
+                        onNewWish({done: false, text: newWishText});
+                        setNewWishText('');
+                    }
+                }}
+            />
     </fieldset>
-);
+    );
+};
+
+WishInput.propTypes = {
+    onNewWish: PropTypes.func,
+};
+
+//En cas de que estigui buida, un valor per defecte
+WishInput.defaultProp = {
+    onNewWish: () => {},
+};
 
 //exportem com a default para que sigui accessible desde index.js
 export default WishInput;
